e2aec96abacc528e21eb319f1ca0d7d488a21c7f
Humildeficador <jvictor.leandro@gmail.com>
2026-06-19 14:01:20 -0300

    chore(backend): add initial Fastify, Prisma with SQLite and Docker setup

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import cors from '@fastify/cors'
+import fastify from 'fastify'
+import { users } from './routes/users'
+
+const app = fastify()
+
+app.register(cors, {
+	origin: '*',
+})
+
+app.register(users, { prefix: '/' })
+
+export { app }
