5bf0789404cf8568890036d549799f95037ced28
Humildeficador <jvictor.leandro@gmail.com>
2026-06-20 17:26:28 -0300

    chore(api): setup fastify swagger and swagger-ui with zod provider

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,38 @@
 import cors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 import { setupErrorHandler } from './handlers/errorHandler'
 import { users } from './routes/users'
 
 const app = fastify()
 
-setupErrorHandler(app)
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'WeSplit API',
+      description: 'Documentação do back-end',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+setupErrorHandler(app)
 app.register(cors, {
-	origin: '*',
+  origin: '*',
 })
 
 app.register(users, { prefix: '/users' })
