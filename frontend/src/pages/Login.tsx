ef4976efd86ad6a803f64e3c21e28a9a8d7946ff
Filipuci <rafaellifipuci@outlook.com>
2026-06-23 07:18:07 -0300

    feat(login): add login page layout structure

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,19 @@
+import { LoginForm } from "../components/Login/LoginForm"
+import { LoginHeader } from "../components/Login/LoginHeader"
+import { LoginHero } from "../components/Login/LoginHero"
+import { PhoneMockup } from "../components/Login/PhoneMockup"
+
 export const Login = () => {
   return (
-    <div>
-    
+    <div className="bg-background text-white h-dvh">
+      <div className="container mx-auto h-full flex flex-col">
+        <LoginHeader />
+        <div className="flex relative">
+          <LoginHero />
+          <LoginForm />
+          <PhoneMockup />
+        </div>
+      </div>
     </div>
   )
 }
