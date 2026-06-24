ef661e572c8d81c539196b84e2b687047ee89c50
Filipuci <rafaellifipuci@outlook.com>
2026-06-24 10:47:36 -0300

    fix(login): add select-none and pointer-events-none in phone mockup

--- frontend/src/components/Login/PhoneMockup.tsx
+++ frontend/src/components/Login/PhoneMockup.tsx
@@ -1,7 +1,7 @@
 export const PhoneMockup = () => {
   return (
-    <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
-      <img src="/images/blue-mobile.png" className="w-100 [filter:drop-shadow(0_0_40px_rgba(69,128,196,0.5))]"/>
+    <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 select-none pointer-events-none">
+      <img src="/images/blue-mobile.png" className="w-100 filter-[drop-shadow(0_0_40px_rgba(69,128,196,0.5))]"/>
     </div>
   )
 }
